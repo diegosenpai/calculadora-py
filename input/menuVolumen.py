c68cc1813c70143ea7d779b2c3e220d6fcc2affe
usuario <abc@google.com>
2023-10-12 15:45:26 -0500

    Avence Menu volumen

---
+++ input/menuVolumen.py
@@ -0,0 +1,25 @@
+from conversor import volumen
+from excepciones import ExcepcionMenu
+
+def subMenuVolumen():
+    menuVol = 0
+    while True:
+        try:
+            print("::: Conversor Volumen :::")
+            print("1. Convertir Galones a Metros Cubicos")
+            print("2. Convertir Litros a Galones")            
+            opcionVol = input(">>Ingrese su opcion \n")
+            menuVol = int(opcionVol)
+            break
+        except ValueError as error:
+            print(error)
+    if menuVol == 1:
+        valorGalones = float(input("Ingrese el valor en Galones:\n"))
+        valorMetros = volumen.convertirGalonesAMetrosCubicos(valorGalones)
+        print(f"{valorGalones:.2f} galones equivale a {valorMetros:.2f} metros cúbicos")
+    elif menuVol ==2:
+        valorLitros = float(input("Ingrese el valor en litros:\n"))
+        valorGalones = volumen.convertirLitrosAGalones(valorLitros)
+        print(f"{valorLitros:.2f} litros equivale a {valorGalones:.2f} galones")    
+    else:
+        raise ExcepcionMenu(f"Sub-Menu Volumen: {menuVol} no es una opcion válida")
